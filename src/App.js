e871391d45ca3f6e94cb1733d106f5418cf417f5
Fahreza Pasha Haikal <68259238+fphaikal@users.noreply.github.com>
2022-10-30 20:46:57 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
